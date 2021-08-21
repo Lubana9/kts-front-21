dafa10b2241eee4c537c4fd41a8a817d0c58f0ee
Lubana <lubanabu98@gmail.com>
2021-08-21 14:12:23 +0300

    ApiStore correction

--- src/hw/src/shared/store/ApiStore/ApiStore.ts
+++ src/hw/src/shared/store/ApiStore/ApiStore.ts
@@ -1,45 +1,36 @@
-  
+
 import {ApiResponse, IApiStore, RequestParams} from "./types";
 
 export default class ApiStore implements IApiStore {
-   baseUrl = '${this.baseUrl}/orgs/${orgName}/repos';
-   async request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>>{
+    constructor() {
 
-    return await fetch(this.baseUrl, {
-      method:params.method,
-      body: JSON.stringify(params.endpoint),
-      headers: params.headers
-  }).then(response => {
-      if(response.ok)
-      return response.json()
-  })
-    
+      this.baseUrl = '${this.baseUrl}/orgs/${orgName}/repos';
+      
+    }
+  baseUrl: string;
 
-
-
-  // не получились (((
-  /* try {
-    const response = await fetch(this.baseUrl, {
-      method:params.method,
-      body: JSON.stringify(params.endpoint),
-      headers: params.headers
-  });
-  
-    const data = await response.json();
-  
-    return {
-      success: true,
-      data,
-      status: StatusHTTP.ok
-    };
-  } catch (e) {
-    return {
-      success: false,
-      data: e,
-      status: StatusHTTP.NOT_FOUND
-    };
-  } */
-
-
-}}
+   async request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>> {
+      try {
+        const response = await fetch(this.baseUrl, {
+          method:params.method,
+          body: JSON.stringify(params.endpoint),
+          headers: params.headers
+      });
+      
+        const data = await response.json();
+      
+        return {
+          success: true,
+          data,
+          status: data
+        };
+      } catch (e) {
+        return {
+          success: false,
+          data: e,
+          status: e
+        };
+      } 
+    }
+}
 
