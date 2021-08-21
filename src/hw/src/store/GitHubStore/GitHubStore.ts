5053cac0c4b5a1e6c480be061c7ba7547e69be5f
Lubana <lubanabu98@gmail.com>
2021-08-21 14:55:47 +0300

    creating Githup Store

--- src/hw/src/store/GitHubStore/GitHubStore.ts
+++ src/hw/src/store/GitHubStore/GitHubStore.ts
@@ -2,11 +2,22 @@ import ApiStore from '../../shared/store/ApiStore';
 import {IGitHubStore} from "./types";
 
 export default class GitHubStore implements IGitHubStore {
-    private readonly apiStore = new ApiStore(); // TODO: не забудьте передать baseUrl в конструктор
+    private readonly apiStore = new ApiStore();
+    baseUrl: string;
+    constructor() {
+
+        this.baseUrl = '${this.baseUrl}/orgs/${orgName}/repos';
+        
+      }
+    // TODO: не забудьте передать baseUrl в конструктор
 
     // TODO: реализовать интерфейс IGitHubStore
 
     async getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResp<RepoItem[]>> {
+     
+          const response = await this.apiStore.request(params.getOrganizationReposList);
+          return response;
+        
         // TODO: Здесь сделайте вызов из this.apiStore и верните результат
         // Документация github: https://docs.github.com/en/rest/reference/repos#list-organization-repositories
     }
