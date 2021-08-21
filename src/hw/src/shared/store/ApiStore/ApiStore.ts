bafe684f18b7b949857b6ec295abe5fdd279dcdc
Lubana <lubanabu98@gmail.com>
2021-08-21 12:25:55 +0300

    some changes

--- src/hw/src/shared/store/ApiStore/ApiStore.ts
+++ src/hw/src/shared/store/ApiStore/ApiStore.ts
@@ -2,19 +2,44 @@
 import {ApiResponse, IApiStore, RequestParams} from "./types";
 
 export default class ApiStore implements IApiStore {
-   baseUrl = 'https://api.github.com/orgs/octo-org/repos';
-   request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>>{
-    const headers ={
-        'Content-Type': 'application-json'
-    }
-    return fetch(this.baseUrl, {
-        method:params.method,
-        body: JSON.stringify(params.endpoint),
-        headers
-    }).then(response => {
-        if(response.ok)
-        return response.json()
-    })
+   baseUrl = '${this.baseUrl}/orgs/${orgName}/repos';
+   async request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>>{
+
+    return await fetch(this.baseUrl, {
+      method:params.method,
+      body: JSON.stringify(params.endpoint),
+      headers: params.headers
+  }).then(response => {
+      if(response.ok)
+      return response.json()
+  })
+    
+
+
+
+  // не получились (((
+  /* try {
+    const response = await fetch(this.baseUrl, {
+      method:params.method,
+      body: JSON.stringify(params.endpoint),
+      headers: params.headers
+  });
+  
+    const data = await response.json();
+  
+    return {
+      success: true,
+      data,
+      status: StatusHTTP.ok
+    };
+  } catch (e) {
+    return {
+      success: false,
+      data: e,
+      status: StatusHTTP.NOT_FOUND
+    };
+  } */
+
 
 }}
 
---
+++ src/hw/src/store/GitHubStore/GitHubStore.ts
@@ -0,0 +1,8 @@
+
+import {IGitHubStore} from "./types";
+
+export default class GitHubStore implements IGitHubStore {
+    getSomeData(params: GetSomeDataParams): Promise<ApiResp<GetSumeDataResp>>{
+              
+    }
+}
