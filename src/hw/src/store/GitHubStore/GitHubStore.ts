0573bf82f84404d518608aa2ef49ccf528a716f3
Lubana <lubanabu98@gmail.com>
2021-08-30 19:36:29 +0300

    adding files

--- src/hw/src/store/GitHubStore/GitHubStore.ts
+++ src/hw/src/store/GitHubStore/GitHubStore.ts
@@ -1,20 +1,22 @@
-import ApiStore from '../../shared/store/ApiStore';
-import { ApiResponse, HTTPMethod } from '../../shared/store/ApiStore/types';
-import {GetOrganizationReposListParams, IGitHubStore, RepoItem} from "./types";
-
-const baseUrl = ' https://api.github.com';
+import ApiStore from "../../shared/store/ApiStore";
+import { ApiResponse, HTTPMethod } from "../../shared/store/ApiStore/types";
+import {
+  GetOrganizationReposListParams,
+  IGitHubStore,
+  RepoItem,
+} from "./types";
+const baseUrl = " https://api.github.com";
 export default class GitHubStore implements IGitHubStore {
-    private readonly apiStore = new ApiStore(baseUrl);
+  private readonly apiStore = new ApiStore(baseUrl);
 
-    async getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResponse<RepoItem[], any>> {
-     
-      return await this.apiStore.request({
-        method: HTTPMethod.get,
-        data: {},
-        headers: {},
-        endpoint:`/orgs/${params.organizaionName}/repos`
-      });
-        
-
-    }
+  async getOrganizationReposList(
+    params: GetOrganizationReposListParams
+  ): Promise<ApiResponse<RepoItem[], any>> {
+    return await this.apiStore.request({
+      method: HTTPMethod.get,
+      data: {},
+      headers: {},
+      endpoint: `/orgs/${params.organizaionName}/repos`,
+    });
+  }
 }
