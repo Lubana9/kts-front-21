69d5233297e512928bda680c301e887c87ee1ea6
Lubana <lubanabu98@gmail.com>
2021-08-31 22:51:51 +0300

    card

--- src/hw/src/App.tsx
+++ src/hw/src/App.tsx
@@ -1,13 +1,14 @@
 
 import Avatar from '@components/avatar';
 import Button from '@components/button';
+import RepoTile from '@components/repoTile';
 
 import './App.css';
 
 function App() {
   return (
     <div >
-      <Avatar/>
+      <RepoTile/>
     </div>
   );
 }
---
+++ src/hw/src/components/repoTile/repoTile.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import SatrIcon from "@components/starIcon/index";
+import { RepoItem } from "@store/GitHubStore/types";
+import Avatar from "@components/avatar/avatar";
+import "./style.css"
+
+
+export type RepoTileProps = {
+    item: RepoItem,
+}
+const RepoTile: React.FC<RepoItem> = (item) => {
+    
+    const handelClick = () => {
+        console.log('clicked');
+    }
+
+    const handelName = () => {
+        console.log(item.name)
+    }
+    return (
+        <>
+        <div
+            onClick={handelClick}
+            className="card card-primary">
+            <Avatar />
+            <div className="card-body">
+                <h4 className="card-heading">{handelName }</h4>
+                <div className="card-link"> <a className="card-link_txt " href="#">kts</a></div>
+                <div className="card-content ">
+                    <SatrIcon />
+                    <span className="card-content_txt"> `${} &ensp; ${}` </span>
+                </div>
+            </div>
+        </div>
+    </>);
+}
+
+export default RepoTile;
