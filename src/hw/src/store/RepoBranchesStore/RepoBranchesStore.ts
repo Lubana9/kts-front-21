6c6f2bfa52f21901da911387cb7bef86eb061ce7
Lubana <86047599+Lubana9@users.noreply.github.com>
2021-09-26 16:47:08 +0300

    Update RepoBranchesStore.ts
    RepoBranchStore

--- src/hw/src/store/RepoBranchesStore/RepoBranchesStore.ts
+++ src/hw/src/store/RepoBranchesStore/RepoBranchesStore.ts
@@ -1,8 +1,81 @@
+import ApiStore from "@store/ApiStore";
+import { HTTPMethod } from "@store/ApiStore/types";
+import {
+  normalaizRepoBranches,
+  RepoBranchesApi,
+  RepoBranchesModel,
+} from "@store/modules/github/repoBranches";
+import {
+  CollectionModel,
+  getIniitCollectionModels,
+  linearizedCollection,
+  normalaizCollection,
+} from "@store/modules/shared/collection";
+import { Meta } from "@utils/Meta";
 import { ILocalStore } from "@utils/UseLocalStore";
+import {
+  action,
+  computed,
+  makeObservable,
+  observable,
+  runInAction,
+} from "mobx";
 
-export default class RepoBranchesStore implements ILocalStore {
-  getDetails(reponame: string) {
-    return reponame;
+import { GetOrganizationReposListParams, IGitHubStore } from "./types";
+
+const BASE_URL = "https://api.github.com";
+type PrivateFilds = "_branches" | "_meta";
+export default class RepoBranchStore implements IGitHubStore, ILocalStore {
+  private readonly apiStore = new ApiStore(BASE_URL);
+  private _meta: Meta = Meta.initial;
+  private _branches: CollectionModel<number, RepoBranchesModel> =
+    getIniitCollectionModels();
+
+  constructor() {
+    makeObservable<RepoBranchStore, PrivateFilds>(this, {
+      _branches: observable.ref,
+      _meta: observable,
+      metaBranches: computed,
+      branches: computed,
+      getOrganizationReposList: action,
+    });
   }
-  destroy(): void {}
+  get metaBranches(): Meta {
+    return this._meta;
+  }
+  get branches(): RepoBranchesModel[] {
+    return linearizedCollection(this._branches);
+  }
+  async getOrganizationReposList(
+    params: GetOrganizationReposListParams
+  ): Promise<void> {
+    this._meta = Meta.loading;
+    this._branches = getIniitCollectionModels();
+    const response = await this.apiStore.request<RepoBranchesApi[]>({
+      method: HTTPMethod.get,
+      data: {},
+      headers: {},
+      endpoint: `repos/ktsstudio/${params.organizaionName}/branches`,
+    });
+    runInAction(() => {
+      if (response.success) {
+        try {
+          const branches: RepoBranchesModel[] = [];
+          for (const item of response.data) {
+            branches.push(normalaizRepoBranches(item));
+          }
+          this._meta = Meta.succses;
+          this._branches = normalaizCollection(branches, (item) => item.sha);
+          return;
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.log(e);
+          this._meta = Meta.error;
+          this._branches = getIniitCollectionModels();
+        }
+      }
+    });
+  }
+
+  destroy() {}
 }
